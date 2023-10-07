0d93bf9c904ab1646a9aa6d481dd5ee29d9c5042
wouris <wouris44@gmail.com>
2023-10-07 15:14:20 +0000

    Added No headers custom exception

---
+++ src/main/java/sk/kasv/mrazik/fitfusion/exceptions/classes/NoHeadersException.java
@@ -0,0 +1,20 @@
+package sk.kasv.mrazik.fitfusion.exceptions.classes;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import sk.kasv.mrazik.fitfusion.models.classes.ErrorResponse;
+import sk.kasv.mrazik.fitfusion.utils.GsonUtil;
+
+@ControllerAdvice
+public class NoHeadersException extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String error = GsonUtil.getInstance().toJson(new ErrorResponse(ex.getMessage()));
+        return new ResponseEntity<>(error, headers, status);
+    }
+}
